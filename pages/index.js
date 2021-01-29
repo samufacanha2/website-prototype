9b183990d9eb3b41f3f2f6b6ea872dcf4bed2b0c
samuel façanha <samufacanha@gmail.com>
2021-01-29 15:52:41 -0300

    Teste Link

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
-function Home(){
-    return <div>
-                <h1>Home</h1>
-            </div>
+import Link from 'next/link';
+
+export default function Home() {
+    return (<div>
+    <h1 >Home</h1>
+    <Link href="/sobre">
+    <a>sobre</a>
+    </Link>
+        </div>)
 
 }
 
-export default Home
---
+++ pages/sobre.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function Sobre(){
+    return <div>
+        <h1>Sobre</h1>
+        <Link href="/">
+        <a>home</a>
+        </Link>
+    </div>
+
+}
+
