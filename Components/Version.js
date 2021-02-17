55aa040f4c9afa153abc779c3b399390fb133684
samuel façanha <samufacanha@gmail.com>
2021-02-17 13:59:15 -0300

    Version Update v0.7 - types + version bugfix

--- Components/Version.js
+++ Components/Version.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Version({children}) {
     return ( <div>
             {children}
-            <div className="version">(Em Desenvolvimento) Versão 0.5</div>
+            <div className="version">(Em Desenvolvimento) Versão 0.6</div>
         </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -5,42 +5,42 @@ import Image from 'next/image';
 
 export default function Page(){
 
-   return <body>
+   return <div>
    <div id="__next">
-       <div class="container">
+       <div className="container">
            <header>
-               <h1 class= 'title'>Olá, eu sou <br/><span class='title'>Samuel Façanha</span></h1>
-               <p class="description">E essa é minha página pessoal feita em Next.js!</p>
+               <h1 className= 'title'>Olá, eu sou <br/><span className='title'>Samuel Façanha</span></h1>
+               <p className="description">E essa é minha página pessoal feita em Next.js!</p>
            </header>
 
-           <img src='/images/perfil.jpg' class='avatar' />
+           <img src='/images/perfil.jpg' className='avatar' />
            <div id="GridM">
                <Link href="/projetos">
-               <a class="card">
+               <a className="card">
                    <h3>Projetos</h3>
                    <p>APPs WEB, GitHub, etc.</p>
                </a>
                </Link>
                <Link href="/contato">
-               <a class="card">
+               <a className="card">
                     <h3>Contato</h3>
                     <p>E-mail, Instagram, etc.</p>
                </a>
                </Link>
                <Link href="/profissional">
-               <a class="card">
+               <a className="card">
                     <h3>Profissional</h3>
                     <p>Currículo, estágios, etc.</p>
                </a>
                </Link>
                {/* <Link href="#">
-               <a class="card">
+               <a className="card">
                     <h3>Card4</h3>
                     <p>Text Card4</p>
                </a>
                </Link>
                <Link href="#">
-               <a class="card">
+               <a className="card">
                     <h3>Card5</h3>
                     <p>Text Card5</p>
                </a>
@@ -49,6 +49,6 @@ export default function Page(){
        </div>
    </div>
   
-</body>
+</div>
 
 }
