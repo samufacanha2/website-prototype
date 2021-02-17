9d6b574c6899f2c04ce8226862c305a4fd2f20f9
samuel façanha <samufacanha@gmail.com>
2021-02-17 13:31:02 -0300

    Version Update v0.6 - layout + corrected version bug

--- Components/Version.js
+++ Components/Version.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Version({children}) {
     return ( <div>
             {children}
-            <div class="version">(Em Desenvolvimento) Versão 0.4</div>
+            <div className="version">(Em Desenvolvimento) Versão 0.5</div>
         </div>
     )
 }
